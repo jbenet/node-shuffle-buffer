6fe89dbc0c5676d8e2cf973d4440aaeb0207925b
Juan Batiz-Benet <juan@benet.ai>
2014-06-08 15:59:25 -0700

    - underscore + lodash.shuffle closes #1

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var _ = require('underscore')
+var shuffle = require('lodash.shuffle')
 
 module.exports = function(buf) {
   if (!(buf instanceof Buffer))
     throw new Error('buf should be a Buffer')
 
-  return new Buffer(_.shuffle(buf.toJSON()))
+  return new Buffer(shuffle(buf.toJSON()))
 }
